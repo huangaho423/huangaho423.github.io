0f75c58bfafa829d2823ea5e3a786f0e2a16e4ef
DaviD <45256227+nccudh@users.noreply.github.com>
2020-04-04 00:42:11 +0800

    新增作品

---
+++ project/fitness-band/precache-manifest.4f18e3566c236abe1f071a96879cab9e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be3ef747f3f96ad12c5d51d63b0248c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c34d013fc92a002aaab2",
+    "url": "./static/css/main.7523ba7c.chunk.css"
+  },
+  {
+    "revision": "f03b7f739c3b918c277a",
+    "url": "./static/js/2.ecfa9846.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "./static/js/2.ecfa9846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c34d013fc92a002aaab2",
+    "url": "./static/js/main.a4cb5de8.chunk.js"
+  },
+  {
+    "revision": "ba4f41cc5508f2fce4f6",
+    "url": "./static/js/runtime-main.a9428b51.js"
+  },
+  {
+    "revision": "0369dc3cc8bc0b03988d3e566f78464e",
+    "url": "./static/media/album01.0369dc3c.jpg"
+  },
+  {
+    "revision": "245fcf40137eed7c6ebdbceed599371e",
+    "url": "./static/media/album02.245fcf40.jpg"
+  },
+  {
+    "revision": "ea33610b2f093bedced9b95208c5d5d2",
+    "url": "./static/media/album03.ea33610b.jpg"
+  },
+  {
+    "revision": "0d009b964a3902ba424fc251715f02a3",
+    "url": "./static/media/album05.0d009b96.jpg"
+  },
+  {
+    "revision": "bfb8ad2380ae813e873f7c1d232c9ee9",
+    "url": "./static/media/album06.bfb8ad23.jpg"
+  },
+  {
+    "revision": "417f9df9ecb86c385736dae773bc9bf3",
+    "url": "./static/media/album07.417f9df9.jpg"
+  },
+  {
+    "revision": "51588ddc737c3fe2f8e4bd817f373650",
+    "url": "./static/media/album08.51588ddc.jpg"
+  },
+  {
+    "revision": "b959de9a66a21a7cc5099f98e3d9f315",
+    "url": "./static/media/album09.b959de9a.jpg"
+  },
+  {
+    "revision": "a8b4156274af7c45e2ba5e20346ae949",
+    "url": "./static/media/band-sprite.a8b41562.jpg"
+  },
+  {
+    "revision": "fc87d4153dca0a316c9b6e430a777637",
+    "url": "./static/media/bg1.fc87d415.jpg"
+  },
+  {
+    "revision": "b6d853c5f04eae781d02d550b62f5d5e",
+    "url": "./static/media/clock.b6d853c5.png"
+  },
+  {
+    "revision": "d2788c28764f8cf3ee4a3b543fc7d6be",
+    "url": "./static/media/heartbeat.d2788c28.png"
+  },
+  {
+    "revision": "765f9090a9a11267e051fd8afa1c5641",
+    "url": "./static/media/video.765f9090.mp4"
+  }
+]);
